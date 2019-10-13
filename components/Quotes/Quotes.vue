9224b0d6081831d2f83157ac0c73139e20c64a29
Alex Litvinov <litvinov1618@gmail.com>
2019-10-13 16:26:51 +0300

    first

---
+++ components/Quotes/Quotes.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="quote">
+        <h2>Цитата дня</h2>
+        <div>
+            <p>„ {{ quotes[quoteCounter].text }} “</p>
+            <span>Автор: {{quotes[quoteCounter].name }}</span>
+        </div>
+        <div class="quote-btn">
+            <input type="button" value="Предыдущая цитата"  @click="prevQuote">
+            <input type="button" value="Следующая цитата" @click="nextQuote">
+            <input type="button" value="Показать все цитаты" @click="showAll" v-if="!allQuotes">
+            <input type="button" value="Скрыть все цитаты" @click="hideAll" v-if="allQuotes">
+        </div>
+        <span>Кличество цитат: {{ quoteCounter + 1 }} / {{ quotes.length }}</span>
+        <div v-if="allQuotes">
+           <ol>
+                <li v-for="(quote, idx) in quotes" :key="idx">
+                    <p>{{quote.text}}</p>
+                    <span>{{quote.name}}</span>
+               </li>
+           </ol>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+    export default {
+
+    }
+</script>
