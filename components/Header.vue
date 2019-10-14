30923377fc9c678b4fada1ee1ff84a9fd109b4fb
Alex Litvinov <litvinov1618@gmail.com>
2019-10-15 00:17:29 +0300

    second try

---
+++ components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>My Awesome app on Vue JS</h1>
+</template>
+
+<style scoped>
+    h1 {
+        font-family: Arial, Helvetica, sans-serif;
+    }
+</style>
---
+++ components/Quotes/AllQuotes.vue
@@ -0,0 +1,25 @@
+<template>
+    <li class="quotes__item">
+        <p>"{{quote.text}}"</p>
+        <small>{{quote.name}}</small>
+    </li>
+</template>
+
+<style lang="scss" scoped>
+    .quotes__item {
+        font-size: 18px;
+        padding-bottom: 10px;
+
+        small {
+            color: grey;
+        }
+    }
+</style>
+
+
+<script>
+    export default {
+        props: ['quote']
+    }
+</script>
+
